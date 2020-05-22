523333c13bb5c1b1add16ea9a47a18683089d2d2
JDat <jdat@inbox.lv>
2020-05-22 18:02:32 +0300

    remove binaries

--- dvb_char_driver/.datv-module.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/jdat/develop/krnl_drv/dvb_char_driver/datv-module.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/jdat/develop/krnl_drv/dvb_char_driver/datv-module.ko /home/jdat/develop/krnl_drv/dvb_char_driver/datv-module.o /home/jdat/develop/krnl_drv/dvb_char_driver/datv-module.mod.o ;  true
--- dvb_char_driver/.tmp_versions/datv-module.mod
+++
@@ -1,3 +0,0 @@
-/home/jdat/develop/krnl_drv/dvb_char_driver/datv-module.ko
-/home/jdat/develop/krnl_drv/dvb_char_driver/main.o /home/jdat/develop/krnl_drv/dvb_char_driver/device_file.o /home/jdat/develop/krnl_drv/dvb_char_driver/gpio_irq.o /home/jdat/develop/krnl_drv/dvb_char_driver/dvb_fifo.o
-
